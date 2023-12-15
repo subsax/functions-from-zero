738731949cb07cdad6bce14c3fd4a0ed3cd485ab
subsax <39213779+subsax@users.noreply.github.com>
2023-12-15 08:35:40 +0000

    adding file to repo

---
+++ mystatements.py
@@ -0,0 +1,9 @@
+my_list=["apple","cherry","lemon"]
+my_dict={"drink":"coffe","milk":"choclate"}
+
+for item in my_list:
+    print(f"the values of the list: {item}")
+
+#print the values of dictonaries
+for _,values in my_dict.items():
+    print(f"the values of my dictonary: {values}")
